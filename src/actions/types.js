cc59196015493b52b61e501091c0132cdd3a886c
Juan Domínguez <juan@buziapp.com>
2018-06-05 19:17:43 -0500

    WIP

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const PASSWORD_CHANGED = 'password_changed';
 export const USER_LOGIN_FAIL = 'user_login_fail';
 export const USER_LOGIN_SUCCESS = 'user_login_success';
 export const USER_LOGIN = 'user_login';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection , Input, Button } from './common';
+
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={
+              text => this.props.employeeUpdate({ prop: 'name', value: text })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placheholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={
+              text => this.props.employeeUpdate({ prop: 'phone', value: text })
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(null, { employeeUpdate })(EmployeeCreate);
